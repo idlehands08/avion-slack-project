337b667dd70c8d6917112646e47d5b2b7e6efa84
sun-asterisk <fritzgeralddumdum@github.com>
2021-09-22 09:27:36 +0800

    axios header init

---
+++ src/api/base.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const TOKEN = Cookies.get('access-token');
+const CLIENT = '';
+const EXPIRY = 1626789364;
+const UID = '';
+
+// axios header init to access api
+const API = axios.create({
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
+  headers: {
+    'Content-Type': 'application/json',
+    'access-token': `${TOKEN}`,
+    'client': `${CLIENT}`,
+    'expiry': EXPIRY,
+    'uid': UID
+  }
+});
+
+export default API;
