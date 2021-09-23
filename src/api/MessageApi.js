bc2e9db23c5fe4f347830e5560a8163582c2d20e
sun-asterisk <fritzgeralddumdum@github.com>
2021-09-23 14:36:21 +0800

    change some codes

--- src/api/MessageApi.js
+++ src/api/MessageApi.js
@@ -11,10 +11,10 @@ const MessageApi = {
 
         return API.request(options);
     },
-    retrieve: (headers, params) => {
+    retrieve: (params, headers) => {
         const options = {
             method: 'GET',
-            url: `http://206.189.91.54//api/v1/messages${params}`,
+            url: `/api/v1/messages?${params}`,
             headers: headers
         };
 
