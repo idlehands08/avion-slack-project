5b9ac835e52542fed733e0db53d4d9d0a1a681f8
Sean Dela Cruz <andreadlacrz1@gmail.com>
2021-09-23 22:00:25 +0800

    Get user channel & details

--- src/api/ChannelApi.js
+++ src/api/ChannelApi.js
@@ -9,8 +9,28 @@ const ChannelApi = {
             headers: headers
         };
 
+
         return API.request(options);
-    }
+    },
+    details: (id, headers) => {
+        const options = {
+            method: 'GET',
+            url: `/api/v1/channels/${id}`,
+            headers: headers
+        }
+        return API.request(options);
+    },
+    members: (payload, headers) => { 
+        const options = {
+            method: 'POST',
+            url: '/api/v1/channels',
+            data:payload,
+            headers: headers
+        }
+        return API.request(options);
+    },
+
+
 };
 
 export default ChannelApi;
