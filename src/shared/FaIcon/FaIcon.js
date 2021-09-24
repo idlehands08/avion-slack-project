873274d29dd5edf8b73e673b8125894ca5044b87
sun-asterisk <fritzgeralddumdum@github.com>
2021-09-24 20:25:44 +0800

    fa icons react

--- src/shared/FaIcon/FaIcon.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './FaIcon.scoped.css';
-
-function FaIcon ({ faIcon }) {
-    return <i className={`icons fa fa-${faIcon}`} aria-hidden="true"></i>;
-}
-
-export default FaIcon;
