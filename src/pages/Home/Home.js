56024f96afabbe44d565bb6314c8003010687f86
sun-asterisk <fritzgeralddumdum@github.com>
2021-09-25 14:04:25 +0800

    textarea message component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../../shared/Button/Button';
+import TextArea from '../../shared/TextArea/TextArea';
 import authApi from '../../services/AuthApi'
 
-
+import './Home.scoped.css';
 
 const handleLogout = () => {
     authApi.logout();
-    window.location = '/Login';
-  }
+    window.location = '/login';
+}
+
 
 function Home () {
+    const [value, setValue] = useState('');
+
+    const handleOnChange = (e) => {
+        setValue(e.target.value)
+    }
+
     return (
-        <div className="column full-content">
-            This is our home page
-        <Button text='dummy Logout' handleClick={handleLogout} />
+        <div className="container full-content">
+            <div className="content">
+                This is our home page
+                <Button text='dummy Logout' handleClick={handleLogout} />
+            </div>
+            <TextArea
+                placeholder='Send message...'
+                handleOnChange={handleOnChange}
+                value={value}
+            />
         </div>
         
     )
---
+++ src/shared/TextArea/TextArea.js
@@ -0,0 +1,57 @@
+import React, { useRef, useEffect, useState } from 'react';
+import { RiSendPlane2Fill } from 'react-icons/ri';
+
+import './TextArea.scoped.css';
+
+function TextArea ({ placeholder, handleOnChange, value }) {
+    const textareaRef = useRef(null);
+    const [isFocused, setIsFocused] = useState(false);
+
+    useEffect(() => {
+        const scrollHeight = computeTextHeight(0);
+        textareaRef.current.style.overflow = scrollHeight > window.innerHeight / 2 ? 'auto' : 'hidden';
+    }, [value])
+
+    const handleFocus = () => {
+        computeTextHeight(1)
+        setIsFocused(true);
+    }
+
+    const handleBlur = () => {
+        computeTextHeight(0);
+        setIsFocused(false);
+    }
+
+    const computeTextHeight = (deduct) => {
+        textareaRef.current.style.height = '0px';
+        const scrollHeight = textareaRef.current.scrollHeight;
+        textareaRef.current.style.height = `${scrollHeight - deduct}px`;
+
+        return scrollHeight;
+    }
+
+    const handleSendMessage = () => {
+        computeTextHeight(2);
+        textareaRef.current.focus();
+    }
+
+    return (
+        <div className="textarea-container">
+            <textarea
+                ref={textareaRef}
+                placeholder={placeholder}
+                value={value}
+                onChange={handleOnChange}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+            />
+            <div id="send-container" className={ isFocused && 'focus' }>
+                <button onClick={handleSendMessage}>
+                    <RiSendPlane2Fill />
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default TextArea;
