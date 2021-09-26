976176734bfcfe68207ad6d1ef0460699910789f
idlehands08 <geofftd08@gmail.com>
2021-09-27 04:39:13 +0800

    searchbar functionality

--- src/shared/SearchBar/component/SearchForm.js
+++ src/shared/SearchBar/component/SearchForm.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'; 
-import { NavLink } from "react-router-dom";
+
 import { BiSearch } from 'react-icons/bi';
 import { GrClose } from 'react-icons/gr';
 import './SearchForm.scoped.css';
+import SearchList from './SearchList';
 
 function SearchForm({ handleClick }) {
-    const list = ['Apple', 'Banana', 'Cupcake', 'AFile', 'Bfile', 'Cfile', 'Android', 'Boozy', 'Cooper']
+    
     const [searchInput, setSearchInput] = useState('');
     
     const handleChange = (e) => {
@@ -19,19 +20,7 @@ function SearchForm({ handleClick }) {
              <input type='text' onChange={handleChange} placeholder="Search far and wide"></input>
              <GrClose className="close-icon icon" onClick={handleClick}/>
         </div>
-          <ul>
-          {list.filter((item) => {
-              if (searchInput === '') {
-                    return item
-              } else {
-                    return item.toUpperCase().includes(searchInput.toUpperCase())
-            }
-          }).map((item) => {
-              return (
-                  <li><NavLink className="nav-link" to='/'  > {item} </NavLink></li>
-              );
-          })}
-          </ul>
+        <SearchList searchInput={searchInput} />
       </div>
     )
 }
