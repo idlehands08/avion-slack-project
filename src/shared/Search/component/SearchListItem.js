7b18b0259f627dca5a81538887e2804488903f6c
sun-asterisk <fritzgeralddumdum@github.com>
2021-09-28 03:09:27 +0800

    close on clicking outside component & refactor search field

--- src/shared/Search/component/SearchListItem.js
+++ src/shared/Search/component/SearchListItem.js
@@ -2,10 +2,7 @@ import { NavLink } from "react-router-dom";
 import './SearchListItem.scoped.css';
 
 function SearchListItem ({ item }) {
-    
-    return ( 
-        <li><NavLink className="nav-link" to='/'> {item}</NavLink></li>
-    )
+    return <li><NavLink className="nav-link" to='/'>{ item }</NavLink></li>;
 }
 
 export default SearchListItem;
