77d7c4a7edcfeea5020b11ae690dbd4f9b8ac53f
idlehands08 <geofftd08@gmail.com>
2021-09-30 04:06:16 +0800

    compose message search bar

--- src/shared/Search/SearchList.js
+++ src/shared/Search/SearchList.js
@@ -1,12 +1,12 @@
 import SearchListItem from './SearchListItem'
 import './SearchList.scoped.css';
 
-function SearchList ({ results, searched }) {
+function SearchList ({ results, searched, customClass }) {
 
     return (
         <ul>
-          { results.map((item, index) => item.toLowerCase().includes(searched.toLowerCase()) && 
-          <SearchListItem key={index} item={item} />) }
+          { results.map((item, index) => item.name.toLowerCase().includes(searched.toLowerCase()) && 
+          <SearchListItem key={index} item={item} customClass={customClass} />) }
           <li>Not the results you expected? <a href="#">Give feedback</a> or <a href="#">learn more</a></li>
         </ul>
     )
--- src/shared/Search/SearchListItem.js
+++ src/shared/Search/SearchListItem.js
@@ -1,8 +1,17 @@
 import { NavLink } from "react-router-dom";
+import Image from '../Image/Image';
 import './SearchListItem.scoped.css';
 
-function SearchListItem ({ item }) {
-    return <li><NavLink className="nav-link" to='/'>{ item }</NavLink></li>;
+function SearchListItem ({ item, customClass }) {
+    const imgStyle = { height: '20px', width: '20px', marginRight: '10px', borderRadius: '4px' }
+    return (
+    <li className={customClass}>
+        <NavLink className="nav-link" to='/'>
+            <Image source={ item.image } customStyle={ imgStyle } />
+            { item.name }
+        </NavLink>
+    </li>
+    )
 }
 
 export default SearchListItem;
